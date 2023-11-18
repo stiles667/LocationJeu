549713f41dd282ad2f0d0117a49e1ff651ccf01b
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-18 11:26:51 +0100

    up

--- location/src/composant/Panier.jsx
+++ location/src/composant/Panier.jsx
@@ -4,7 +4,9 @@ import "./Panier.css";
 
 export default function Panier() {
   const [locations, setLocations] = useState([]);
+  const [jeux, setJeux] = useState([]); // Added state for jeux
   const [searchTerm, setSearchTerm] = React.useState("");
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
@@ -25,7 +27,23 @@ export default function Panier() {
       }
     };
 
+    const fetchJeux = async () => {
+      try {
+        const response = await fetch("http://localhost:3002/jeux");
+        if (response.ok) {
+          const data = await response.json();
+          setJeux(data);
+        } else {
+          throw new Error("Failed to fetch jeux data");
+        }
+      } catch (error) {
+        console.error("Error fetching jeux data:", error);
+      }
+    };
+
     fetchLocations();
+    fetchJeux(); // Fetch jeux data
+
   }, []);
 
   return (
@@ -65,26 +83,31 @@ export default function Panier() {
         </div>
       </div>
 
-    <div className="locations-list">
+      <div className="locations-list">
         <h2>Locations</h2>
         <ul>
-            {locations.map((location, index) => {
-                const dateDebut = new Date(location.DateDebut);
-                const dateFin = new Date(location.DateFin);
-                const days = Math.ceil((dateFin - dateDebut) / (1000 * 60 * 60 * 24));
-                const totalPrice = days * location.Prix;
+          {locations.map((location, index) => {
+            const jeu = jeux.find(j => j.JeuxID === location.JeuxID);
+            const dateDebut = new Date(location.DateDebut);
+            const dateFin = new Date(location.DateFin);
+            const days = Math.ceil((dateFin - dateDebut) / (1000 * 60 * 60 * 24));
+            const totalPrice = days * location.Prix;
 
-                return (
-                    <li key={index}>
-                        <p>Nom du jeu: {location.Titre}</p>
-                        <p>Date de début: {dateDebut.toLocaleDateString()}</p>
-                        <p>Date de fin: {dateFin.toLocaleDateString()}</p>
-                        <p>Prix total: {totalPrice} $</p>
-                    </li>
-                );
-            })}
+            return (
+              <li  className ="carr "key={index} style={{backgroundImage: `url(${jeu && jeu.lien_image})`}}>
+              {jeu && (
+                <>
+                  <p>Nom du jeu: {location.Titre}</p>
+                  <p>Date de début: {dateDebut.toLocaleDateString()}</p>
+                  <p>Date de fin: {dateFin.toLocaleDateString()}</p>
+                  <p>Prix total: {totalPrice} $</p>
+                </>
+              )}
+            </li>
+            );
+          })}
         </ul>
-    </div>
+      </div>
     </div>
   );
 }
